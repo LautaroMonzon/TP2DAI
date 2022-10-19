06c637850a6aa39949d0c4f6a67674cb3a089f10
Android Studio <devandroid@ort>
2022-10-19 08:59:50 -0300

    tercero primer pag

--- paginas/home.js
+++ paginas/home.js
@@ -1,11 +1,31 @@
 import { View, Text } from "react-native"
 
-function HomeScreen() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-    );
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import * as Contacts from 'expo-contacts';
+
+function home() {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails],
+        });
+
+        if (data.length > 0) {
+          const contact = data[0];
+          console.log(contact);
+        }
+      }
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Contacts Module Example</Text>
+    </View>
+  );
 }
 
-export default HomeScreen
+export default home;
